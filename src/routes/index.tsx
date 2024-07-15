fe2a311b7f0dc5ce31e52aa08985e52c6f98cf01
alfredomariamilano <alfmarmil@gmail.com>
2024-07-15 11:48:18 +0200

    refactor: Update main container class names for better responsiveness

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,8 @@ import { Photos } from '~/components/Photos'
 
 export default function Home() {
   return (
-    <main class="min-h-vh w-full">
-      <div class="h-vh w-full">
+    <main class="min-h-screen w-full">
+      <div class="h-screen w-full">
         <BigText />
       </div>
 
